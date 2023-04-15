5536026eb7bfb59ff94e082e7144c35034df52e9
SaleemAlmahde <almahdesaleem@gmail.com>
2023-04-15 13:00:31 +0300

    dropdown

--- lib/pages/property_view_page.dart
+++ lib/pages/property_view_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:property_in_homs/bloc/cubits/app_cubit.dart';
+import 'package:property_in_homs/utils/enums/property_state_enum.dart';
 
 class ViewPage extends StatelessWidget {
   const ViewPage({super.key});
@@ -23,8 +22,48 @@ class ViewPage extends StatelessWidget {
                 TextFormField(
                   // controller: ,
                   readOnly: true,
-                  enabled: false,
-                )
+                  enabled: true,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  // controller: ,
+                  readOnly: true,
+                  enabled: true,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  // controller: ,
+                  readOnly: true,
+                  enabled: true,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButton(
+                    isExpanded: true,
+                    value: appCubit.propertyStateEnum.index,
+                    items: [
+                      DropdownMenuItem(
+                        value: PropertyStateEnum.rental.index,
+                        child: Text(PropertyStateEnum.rental.name),
+                      ),
+                      DropdownMenuItem(
+                        value: PropertyStateEnum.sale.index,
+                        child: Text(PropertyStateEnum.sale.name),
+                      ),
+                    ],
+                    onChanged: (int? value) {
+                      appCubit.propretyStateChangedEvent(
+                          value as PropertyStateEnum);
+                    },
+                  ),
+                ),
               ],
             )),
           ),
