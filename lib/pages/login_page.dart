4f2a10defb6e6b6693c38061dfba9597a0022601
FAI-SAL-47 <130310364+FAI-SAL-47@users.noreply.github.com>
2023-04-17 15:42:18 +0300

    ..

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:property_in_homs/components/custom_text_field.dart';
 
+class MyLoginPage extends StatelessWidget {
+  const MyLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Scaffold(
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const SizedBox(height: 10),
+          const Text('WELLCOME BACK :)'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text('You`ve been missed ;('),
+          //Username textfield
+          CustomTextField(
+            myLabelText: 'Username',
+            controller: TextEditingController(),
+            fillColor: Colors.white,
+          ),
+          const SizedBox(height: 10),
+          //Password Text field
+          CustomTextField(
+            myLabelText: 'Password',
+            controller: TextEditingController(),
+            fillColor: Colors.white,
+          ),
+        ]),
+      ),
+    );
+  }
+}
