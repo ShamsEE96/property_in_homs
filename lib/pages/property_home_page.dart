8f9fcb07fec24c3dea276827ad54db607a25d80f
Laila12sh <130299812+Laila12sh@users.noreply.github.com>
2023-04-18 13:29:11 +0300

    .

--- lib/pages/property_home_page.dart
+++ lib/pages/property_home_page.dart
@@ -1,16 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+import 'package:flutter/material.dart';
+import 'package:property_in_homs/utils/colors.dart';
+import 'package:property_in_homs/widgets/category.dart';
+import 'package:property_in_homs/widgets/propertys.dart';
+import 'package:property_in_homs/widgets/search.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: AppColors.seconderyBlueColor,
+        toolbarHeight: 50.0,
+        title: const Text("     حقل تجارب -_-"),
+      ),
+      backgroundColor: AppColors.seconderyBlueColor,
+      body:PageView(
+        
+          children: [
+           Center( search(),) 
+            SizedBox(
+              height: 10.0,
+            ),
+            Categorys(),
+            SizedBox(
+              height: 20.0,
+            ),
+            propertys()
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 1.0,
+        backgroundColor: AppColors.mainBlueColor,
+        selectedItemColor: AppColors.darkNavyColor,
+        unselectedItemColor: AppColors.seconderyBlueColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: "book"),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "addpost"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "profil")
+        ],
+      ),
+    );
   }
 }
