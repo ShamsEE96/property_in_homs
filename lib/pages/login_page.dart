3f6c63c82db5380791724a0d5d463026d813ba8a
SaleemAlmahde <almahdesaleem@gmail.com>
2023-04-29 01:28:12 +0300

    .

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -36,7 +36,7 @@ class LoginPage extends StatelessWidget {
                 const SizedBox(height: 20),
                 const Center(
                   child: Text(
-                    'WELLCOME BACK',
+                    "WELLCOME BACK",
                     style: TextStyle(
                       fontSize: 25,
                       fontWeight: FontWeight.w600,
@@ -48,21 +48,24 @@ class LoginPage extends StatelessWidget {
                   controller: authCubit.loginUsernameController,
                   myValidator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'This field is requiered';
+                      return "This field is requiered";
                     }
                     return null;
                   },
                   fillColor: Colors.white,
-                  myLabelText: 'Your Username',
+                  myLabelText: "Your Username",
                 ),
                 const SizedBox(height: 10),
                 CustomTextField(
-                  myLabelText: 'Password',
+                  myLabelText: "Password",
                   controller: authCubit.loginPasswordController,
                   myValidator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'This field is requiered';
+                      return "This field is requiered";
                     }
+                    // if (value != authCubit.registerPasswordController.text) {
+                    //   return "Passwors is not correct";
+                    // }
                     return null;
                   },
                   fillColor: Colors.white,
@@ -71,7 +74,7 @@ class LoginPage extends StatelessWidget {
                   height: 20,
                 ),
                 CustomButton(
-                    myButtonText: 'login',
+                    myButtonText: "login",
                     myTextColor: AppColors.mainWhiteColor,
                     backgroundColor: AppColors.darkNavyColor,
                     onPressed: () async {
@@ -95,7 +98,7 @@ class LoginPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text(
-                      'dont have an account ? ',
+                      "dont have an account ? ",
                       style: TextStyle(fontSize: 15),
                     ),
                     InkWell(
@@ -109,7 +112,7 @@ class LoginPage extends StatelessWidget {
                         );
                       },
                       child: const Text(
-                        'Register now',
+                        "Register now",
                         style: TextStyle(
                             color: Color.fromARGB(255, 25, 100, 160),
                             decoration: TextDecoration.underline,
